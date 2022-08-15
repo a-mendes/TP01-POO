e5abf1f994ac7ca9e97e079e394f14cbf3082ddb
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2022-08-14 23:21:03 -0300

    Implementação de classe "Livros"
    Atributos e Getters/Setters

---
+++ src/Livro.cpp
@@ -0,0 +1,64 @@
+#include "headers/Livro.h"
+
+Livro::~Livro()
+{}
+
+vector<string> Livro::getEscritores()
+{
+	return this->escritores;
+}
+
+void Livro::setEscritores(vector<string> escritores)
+{
+	this->escritores = escritores;
+}
+
+string Livro::getTitulo()
+{
+	return this->titulo;
+}
+
+void Livro::setTitulo(string titulo)
+{
+	this->titulo = titulo;
+}
+
+vector<string> Livro::getCapitulos()
+{
+	return this->capitulos;
+}
+
+void Livro::setCapitulos(vector<string> capitulos)
+{
+	this->capitulos = capitulos;
+}
+
+int Livro::getAnoPublicacao()
+{
+	return this->anoPublicacao;
+}
+
+void Livro::setAnoPublicacao(int anoPublicacao)
+{
+	this->anoPublicacao = anoPublicacao;
+}
+
+string Livro::getIdiomaOriginal()
+{
+	return this->idiomaOriginal;
+}
+
+void Livro::setIdiomaOriginal(string idiomaOriginal)
+{
+	this->idiomaOriginal = idiomaOriginal;
+}
+
+vector<string> Livro::getKeywords()
+{
+	return this->keywords;
+}
+
+void Livro::setKeywords(vector<string> keywords)
+{
+	this->keywords = keywords;
+}
---
+++ src/headers/Livro.h
@@ -0,0 +1,46 @@
+#ifndef LIVRO_H_
+#define LIVRO_H_
+
+#include <string>
+#include <vector>
+
+//Verificar boa prática
+using namespace std;
+
+/**
+ * Classe abstrata
+ */ 
+class Livro
+{	
+	private:
+		vector<string> escritores;
+		string titulo;
+		vector<string> capitulos;
+		int anoPublicacao;
+		string idiomaOriginal;
+		vector<string> keywords;
+
+	public:
+		//Confirmar questão de classe abstrata e função virtual pura
+		virtual Livro() = 0;
+		~Livro();
+
+		vector<string> getEscritores();
+		void setEscritores(vector<string> escritores);
+
+		string getTitulo();
+		void setTitulo(string titulo);
+
+		vector<string> getCapitulos();
+		void setCapitulos(vector<string> capitulos);
+
+		int getAnoPublicacao();
+		void setAnoPublicacao(int anoPublicacao);
+
+		string getIdiomaOriginal();
+		void setIdiomaOriginal(string idiomaOriginal);
+
+		vector<string> getKeywords();
+		void setKeywords(vector<string> keywords);
+};
+#endif 
