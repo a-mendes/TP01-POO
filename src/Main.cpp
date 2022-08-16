6b208bb4f93c96ae25dcc50004fb59ce1f4cbaef
Ana Laura <88839593+a-mendes@users.noreply.github.com>
2022-08-16 16:08:28 -0300

    Cabeçalho e inicio de funções de leitura

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,59 @@
 #include <iostream>
+#include <string> 
+#include <sstream> 
+#include <algorithm> 
+#include <iterator>
+#include <vector>
+
+#include "headers/Livro.h"
+
+#define QTD_LIVROS 16
+
+using namespace std;
+
+//Verificar escopo adequado para essa função
+void lerBaseDeDados(vector<Livro> *livros);
 
 int main(int argc, char const *argv[])
-{
-	std::cout << "TP01 - POO" << std::endl;
+{                                         
+	cout << "  _____ ____   ___  _           ____   ___   ___  " << endl;
+	cout << " |_   _|  _ \\ / _ \\/ |         |  _ \\ / _ \\ / _ \\ " << endl;
+	cout << "   | | | |_) | | | | |  _____  | |_) | | | | | | |" << endl;
+	cout << "   | | |  __/| |_| | | |_____| |  __/| |_| | |_| |" << endl;
+	cout << "   |_| |_|    \\___/|_|         |_|    \\___/ \\___/ " << endl;
 
+	cout << endl;
+	cout << "Ana Laura Siqueira Mendes - 20.2.4091" << endl;
+	//Acertem os nomes e matricula de vcs amiguinhos
+	cout << "Guilherme Salim Sapequinha" << endl;
+	cout << "Mateus" << endl;
+	cout << "Romulo Junio Vieira Rocha - 21.1.4022" << endl;
+	
+	/**
+	 * Colecao unica de livros
+	 * 		- Contem os livros fornecidos pelos arquivos .txt
+	 */
+	vector<Livro> livros;
+	lerBaseDeDados(&livros);
 	return 0;
+}
+
+//Verificar escopo adequado para essa função
+void lerBaseDeDados(vector<Livro> *livros)
+{
+	for (int i = 0; i < QTD_LIVROS; ++i)
+	{
+		string nomeArquivo = to_string(i+1) + ".txt";
+		ifstream arquivo(nomeArquivo);
+
+		string tipoLivro = getline();
+	}
+}
+
+void split(const string& str, vector<string>& cont, char delim = ' ') 
+{ 
+	stringstream ss(str); 
+	string token;
+	while (getline(ss, token, delim)) 
+	cont.push_back(token);
 }
