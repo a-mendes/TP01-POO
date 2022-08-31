6e197a55db6a2252a14b0ef6e28abf83bbb93445
Mateus Filipe <mateusfilipe2k2@gmail.com>
2022-08-30 21:49:04 -0300

    comeco itens j k l

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,10 @@
 
 bool hasAudioBook(vector<Livro*> &livros, string escritor);
 
+void mostrarOuSalvarColecaoLivro();
+int quantidadeKeywordColecaoLivro(vector<Livro*> &livros, string keyword);
+void mapeamentoColecaoLivro();
+
 int main(int argc, char const *argv[])
 {                                         
 	cout << "  _____ ____   ___  _           ____   ___   ___  " << endl;
@@ -76,9 +80,59 @@ int main(int argc, char const *argv[])
 
 		} break;
 
+		case 'j':{
+			mostrarOuSalvarColecaoLivro();		
+		}
+
+		case 'k':{
+			string keyword;
+			cin >> keyword;
+			//validar Keyword digitada? 
+
+			cout << "Quantidade de livros com Keyword " << keyword << ": " << quantidadeKeywordColecaoLivro(livros, keyword) << endl;		
+		}
+
+		case 'l':{
+			mapeamentoColecaoLivro();		
+		}
+
 	}
 
 	return 0;
+}
+
+void mostrarOuSalvarColecaoLivro(){
+	// Criar uma função que recebe uma unica coleção de livros de todos os tipos e que                             
+	// mostre no terminal ou salve em um arquivo (saida.txt) todos os tipos de livros.                      
+	// Mostrar os dados comuns e específicos do livro conforme o item a). Um argumento                         
+	// passado para a função define qual será a saída. Nesse caso é necessário                         
+	// downcasting.
+}
+
+int quantidadeKeywordColecaoLivro(vector<Livro*> &livros, string keyword){
+	// Dado uma keyword, criar uma função que retorne a quantidade de livros que                       
+	// possuam aquela keyword dentro uma coleção, seja ela qual for (livro, impresso,                     
+	// eletrônico ou audiobook). No main deve ser mostrado a quantidade encontrada.
+
+	//Buscas pela Keyword por todas os compos do livro	
+
+	//verificar qual o tipo do livro
+	//fazer o dawncasting
+	//buscar pela Keyword e incrementar no retorno
+
+	return 1;
+
+}
+
+void mapeamentoColecaoLivro(){
+	// Criar uma função que recebe uma única coleção de livros de todos os tipos, além                           
+	// de uma estrutura capaz de realizar o mapeamento a seguir (No main mostrar o                         
+	// resultado):
+
+	// i. Inglês = ING;
+	// ii. Espanhol = ESP;
+	// iii. Francês = FRS;
+	// iv. Português  = POT.
 }
 
 //Verificar escopo adequado
